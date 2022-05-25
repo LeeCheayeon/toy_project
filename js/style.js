3e6461ae4568d768941b6750b9f326fbfab60bc1
leechaeyeon <yeontachi@gmail.com>
2022-05-25 18:21:47 +0900

    js 분활, style.js 수정하기( 길이값 다시 지정하기)

--- js/style.js
+++ js/style.js
@@ -1,7 +1,15 @@
 import { createMenu } from "./menu.js";
-console.log(createMenu);
+// console.log(createMenu);
 
 // createMenu(menu, menuArr); // 메뉴생성 함수 호출 
 
-import { createElement } from "./submenu.js";
-console.log(createElement);
+import { clickEvent } from "./submenu.js";
+// console.log(createElement);
+
+const menu = document.getElementById("menu");
+console.log(menu);
+menu.style.listStyleType="none"
+const submenu = document.getElementById("submenu");
+console.log(submenu)
+
+
--- js/submenu.js
+++ js/submenu.js
@@ -4,7 +4,7 @@ function box (target, width, height,backgroundColor){
   target.style.backgroundColor = backgroundColor;
 }
 
-export const createElement = function createSubElement (parentdiv){
+function createSubElement (parentdiv){
   const newElement = document.createElement("div");
   const newClickBox = document.createElement("div"); 
   box(newElement, '300px','300px','#c4c4c4');
@@ -15,4 +15,50 @@ export const createElement = function createSubElement (parentdiv){
   newElement.id = "remove"
 };
 
-createElement (submenu);
+// createSubElement (submenu);
+
+export const clickEvent =  menu.addEventListener("click", function(event){
+  let isstatus = true;
+  console.log(isstatus);
+  let getdataset = event.target.dataset.name;
+  console.log(getdataset);
+  if(isstatus === true){
+    console.log("true");
+    createSubElement(submenu);
+  }
+  //   // menu.addEventListener("click",function(event){
+  //     let getdataset = event.target.dataset.name;
+  //     //변수 getdataset은 이벤트 타겟의 데이터셋의 네임이다. 
+  //     console.log(getdataset);
+  //     for(let i =0; i<menus.length; i++){
+  //       if(menus[i].dataset.name === getdataset){
+  //         // 가리키는 이름이 같다면 
+  //         if(isstatus === true){
+  //           //true일때
+  //           createSubElement(submenu);
+  //           console.log("true");
+            
+  //           //**새로 만든 함수에 dataset 추가 --------------------------
+  //           const submenudiv = document.querySelector("#submenu>div");
+  //           createDataset(submenudiv, getdataset);
+            
+  //           console.log(isstatus);
+  //           isstatus=false;
+  //         }
+  //         else {
+  //           //true가 아닐때
+  //           //** 기존 div를 삭제하고 > 새로운 div 생성 --------------------
+  //           Deletediv();
+  //           console.log("false")
+  //           createSubElement(submenu);
+            
+  //           //** 새로 만든 함수에 dataset 추가 ----------------------------
+  //           const submenudiv = document.querySelector("#submenu>div");
+  //           createDataset(submenudiv, getdataset);
+  //         }
+  //       }
+  //     }
+    // })
+})
+
+// clickEvent(menu);
