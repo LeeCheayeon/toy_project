756797db8de33d792a7ec5420c2d0a035d6eceb7
leechaeyeon <yeontachi@gmail.com>
2022-05-26 10:25:17 +0900

    submenu 생성하고 삭제하는 것 까지 완료/지금까지 했던 js 분활 완료

--- js/submenu.js
+++ js/submenu.js
@@ -24,51 +24,26 @@ function Deletediv(){
   reDiv.remove();
 }
 
+// data-name function
+function createDataset (pararentValue , ArrValue){
+  pararentValue.setAttribute('data-name', ArrValue);
+}
+
+let isstatus = true;
 export const clickEvent =  menu.addEventListener("click", function(event){
-  let isstatus = true;
-  console.log(isstatus);
   let getdataset = event.target.dataset.name;
-  console.log(getdataset);
-  if(isstatus === true){
-    console.log("true");
-    createSubElement(submenu);
+    if(isstatus === true){
+      createSubElement(submenu);
+      const submenudiv = document.querySelector("#submenu>div");
+      createDataset(submenudiv, getdataset);
+      console.log(getdataset);
+      isstatus = false;
   }else{
     // isstatus === false;
-    Deletediv();
+    Deletediv(submenu);
+    console.log("false");
+    createSubElement(submenu);
+    const submenudiv = document.querySelector("#submenu>div");
+    createDataset(submenudiv, getdataset);
   }
-  //   // menu.addEventListener("click",function(event){
-  //     let getdataset = event.target.dataset.name;
-  //     //변수 getdataset은 이벤트 타겟의 데이터셋의 네임이다. 
-  //     console.log(getdataset);
-  //     for(let i =0; i<menus.length; i++){
-  //       if(menus[i].dataset.name === getdataset){
-  //         // 가리키는 이름이 같다면 
-  //         if(isstatus === true){
-  //           //true일때
-  //           createSubElement(submenu);
-  //           console.log("true");
-            
-  //           //**새로 만든 함수에 dataset 추가 --------------------------
-  //           const submenudiv = document.querySelector("#submenu>div");
-  //           createDataset(submenudiv, getdataset);
-            
-  //           console.log(isstatus);
-  //           isstatus=false;
-  //         }
-  //         else {
-  //           //true가 아닐때
-  //           //** 기존 div를 삭제하고 > 새로운 div 생성 --------------------
-  //           Deletediv();
-  //           console.log("false")
-  //           createSubElement(submenu);
-            
-  //           //** 새로 만든 함수에 dataset 추가 ----------------------------
-  //           const submenudiv = document.querySelector("#submenu>div");
-  //           createDataset(submenudiv, getdataset);
-  //         }
-  //       }
-  //     }
-    // })
 })
-
-// clickEvent(menu);
