2cfdae94ac5ac8d761d802ada46c396d94afb722
leechaeyeon <yeontachi@gmail.com>
2022-05-26 16:37:27 +0900

    menus를 클릭했을때 imageboxArr[i].length값 만큼 clickbox 생성해줘 하는 중...

--- js/submenu.js
+++ js/submenu.js
@@ -4,24 +4,7 @@ function box (target, width, height,backgroundColor){
   target.style.backgroundColor = backgroundColor;
 }
 
-const ImageBox = {
-  basic : ["./images/base_BASE.svg"],
-  eyes : ["./images/eyes/base_눈_1.svg", "./images/eyes/base_눈_2.svg"],
-  noseMouth : [],
-  eyebrows : [], 
-  backHair: [],
-  bangs :[],
-  ears:[],
-  clothes:[],
-  deco1:[],
-  deco2:[],
-  backdeco: []
-} 
-let ImageBoxArr = Object.values(ImageBox);
-console.log(ImageBoxArr);
-console.log(ImageBoxArr[0].length);
-// imagebox에 데이터셋 이름을 넣어야함.
-// createDataset(ImageBox,getdataset);
+
 
 //서브메뉴 겉박스
 function createSubElement (parentdiv){
@@ -31,11 +14,37 @@ function createSubElement (parentdiv){
   parentdiv.appendChild(newElement);
 };
 
+      const ImageBox = {
+        basic : [1],
+        eyes: [1,2],
+        noseMouth: [],
+        eyebrows: [], 
+        backHair: [],
+        bangs: [],
+        ears: [],
+        clothes: [],
+        deco1: [],
+        deco2: [],
+        backdeco: []
+      }
+
+      const menus = document.querySelectorAll("#menu>div");
+      console.dir(menus);
+
+      let ImageBoxArr = Object.values(ImageBox);
+      console.dir(ImageBoxArr);
+      console.dir(ImageBoxArr[1].length);
+      // for(let i=0; i<ImageBoxArr.length; i++){
+      //   menus[i] = ImageBoxArr[i];
+      // }
+
+
 //서브메뉴 클릭박스 생성
+// 1. 이미지 박스의 길이값만큼 박스를 만들어줘
 function newClickBox (pararentValue){
-  const newClickBox = document.createElement("div"); 
-  box(newClickBox, '50px', '50px', "#ff6666");
-  pararentValue.appendChild(newClickBox);
+      const newClickBox = document.createElement("div"); 
+      box(newClickBox, '50px', '50px', "#ff6666");
+      pararentValue.appendChild(newClickBox);
 }
 
 // 만약에 imagebox의 [i]dhk eventtargetdml i가 같다면? 
@@ -59,46 +68,59 @@ let isstatus = true;
 export const clickEvent =  menu.addEventListener("click", function(event){
   let getdataset = event.target.dataset.name;
   // console.log(getdataset);
-    if(isstatus === true){
-      createSubElement(submenu);
+  if(isstatus === true){
+    createSubElement(submenu);
       const submenudiv = document.querySelector("#submenu>div");
       createDataset(submenudiv, getdataset);
       // for(let index =0; index<ImageBoxArr[index].length; index++){
-      // if(ImageBoxArr[index] === event.target.length){
-      //     newElement.appendChild(newClickBox);
-      //   }
-      // }
-      // console.dir(submenudiv.dataset.name);
-      console.log(getdataset);
-      isstatus = false;
-  }else{
-    // isstatus === false;
-    // console.dir(event.target.childElementCount);
-    Deletediv(submenu);
-    console.log("false");
-    createSubElement(submenu);
-    const submenudiv = document.querySelector("#submenu>div");
-    createDataset(submenudiv, getdataset);
-  }
-})
+        // if(ImageBoxArr[index] === event.target.length){
+          //     newElement.appendChild(newClickBox);
+          //   }
+          // }
+          // console.dir(submenudiv.dataset.name);
+          console.log(getdataset);
+          isstatus = false;
+        }else{
+          // isstatus === false;
+          // console.dir(event.target.childElementCount);
+          Deletediv(submenu);
+          console.log("false");
+          createSubElement(submenu);
+          const submenudiv = document.querySelector("#submenu>div");
+          createDataset(submenudiv, getdataset);
+        }
+      })
+      
+      // 의사코드 
+      //1. clickBox.dataset.name 
 
-  menu.addEventListener("click", function(evnet){
-    let getdataset = evnet.target.dataset.name;
-    const submenudiv = document.querySelector("#submenu>div");
-    let submenuDataset = submenudiv.dataset.name;
-    console.log(getdataset);
-    console.log(submenudiv.dataset.name);
-    console.dir(evnet.target);
-    console.dir(ImageBoxArr[0]);
-    if(getdataset === submenuDataset){
-      // 만약 getdataset === submenuDataset 같다면
-      // 새로만든 클릭박스의 데이터셋 이름을 타켓인 디이터셋 이름과 같은걸로 달아줘. 
-      submenudiv.style.backgroundColor = "green";
-      // 클릭 박스를 imagebox의 [i].length 값만큼 만들어줘.
-      // if()
-      // 만약 00과 00이 같다면 
-      newClickBox(submenudiv);
 
+      menu.addEventListener("click", function(evnet){
+        let getdataset = evnet.target.dataset.name;
+        const submenudiv = document.querySelector("#submenu>div");
+        let submenuDataset = submenudiv.dataset.name;
+        console.log(getdataset);
+        console.log(submenudiv.dataset.name);
+        console.dir(evnet.target);
+        console.dir(ImageBoxArr[0]);
+        if(getdataset === submenuDataset){
+          // 만약 getdataset === submenuDataset 같다면
+          // 새로만든 클릭박스의 데이터셋 이름을 타켓인 디이터셋 이름과 같은걸로 달아줘. 
+          newClickBox(submenudiv);
+          let clickBox = document.querySelector("#submenu>div>div");
+          createDataset(clickBox, getdataset);
+          console.dir(clickBox.dataset.name);
+          // 클릭후 dataname이 들어가긴함. 
+          for(let i=0; i<ImageBoxArr.length; i++){
+            
+            console.dir(ImageBoxArr[i].length);
+            if(clickBox.dataset.name === ImageBoxArr[i]){
+        console.log('true');
+        console.log(ImageBoxArr[i]);
+        newClickBox(submenudiv);
+      }
+      }
+      // 만약 데이터 클릭박스의 이름이 메뉴 어레이의 값과 같다면 
     }
-
+    
   })
