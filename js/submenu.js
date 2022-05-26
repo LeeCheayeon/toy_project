8e423fdbc6c71d4fc0ba1d754217320c5f883f91
leechaeyeon <yeontachi@gmail.com>
2022-05-26 11:36:31 +0900

    issue2 : 클릭박스를 이미지박스만큼 만들어줘 생성중

--- js/submenu.js
+++ js/submenu.js
@@ -4,16 +4,35 @@ function box (target, width, height,backgroundColor){
   target.style.backgroundColor = backgroundColor;
 }
 
+const ImageBox = {
+  basic : ["./images/base_BASE.svg"],
+  eyes : ["./images/eyes/base_눈_1.svg", "./images/eyes/base_눈_2.svg"],
+  noseMouth : [],
+  eyebrows : [], 
+  backHair: [],
+  bangs :[],
+  ears:[],
+  clothes:[],
+  deco1:[],
+  deco2:[],
+  backdeco: []
+} 
+let ImageBoxArr = Object.values(ImageBox);
+console.log(ImageBoxArr);
+console.log(ImageBoxArr[0].length);
+
 function createSubElement (parentdiv){
   const newElement = document.createElement("div");
-  const newClickBox = document.createElement("div"); 
+  newElement.id = "remove"
   box(newElement, '300px','300px','#c4c4c4');
+  const newClickBox = document.createElement("div"); 
   box(newClickBox, '50px', '50px', "#ff6666");
-
   newElement.appendChild(newClickBox);
   parentdiv.appendChild(newElement);
-  newElement.id = "remove"
 };
+
+
+
 
 // createSubElement (submenu);
 
@@ -32,10 +51,12 @@ function createDataset (pararentValue , ArrValue){
 let isstatus = true;
 export const clickEvent =  menu.addEventListener("click", function(event){
   let getdataset = event.target.dataset.name;
+  // console.log(getdataset);
     if(isstatus === true){
       createSubElement(submenu);
       const submenudiv = document.querySelector("#submenu>div");
       createDataset(submenudiv, getdataset);
+      // console.dir(submenudiv.dataset.name);
       console.log(getdataset);
       isstatus = false;
   }else{
@@ -47,3 +68,22 @@ export const clickEvent =  menu.addEventListener("click", function(event){
     createDataset(submenudiv, getdataset);
   }
 })
+
+// console.log(event.target.dataset.name);
+// console.log(submenudiv.dataset.name); // basic
+// if(target.dataset.name === submenudiv.dataset.neme){
+  // }
+  
+  menu.addEventListener("click", function(evnet){
+    let getdataset = evnet.target.dataset.name;
+    const submenudiv = document.querySelector("#submenu>div");
+    let submenuDataset = submenudiv.dataset.name;
+    console.log(getdataset);
+    console.log(submenudiv.dataset.name);
+    if(getdataset === submenuDataset){
+      // 만약 getdataset === submenuDataset 같다면
+      submenudiv.style.backgroundColor = "green";
+      // 클릭 박스를 imagebox의 [i].length 값만큼 만들어줘.
+    }
+
+  })
