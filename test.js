fcc73cc0251171204e8afd182ced998051818d1c
leechaeyeon <yeontachi@gmail.com>
2022-05-31 12:30:25 +0900

    동적생성.html 추가, 어떻게 하면 좋을지 연구중

---
+++ test.js
@@ -0,0 +1,6 @@
+let datasetName = ["나는", "날개달린", "호랑이다", "코딩", "쯤이야", "껌이지"];
+let imageName = ["a", "b", "c", "b", "e", "f"];
+
+datasetName.forEach((value, index) => {
+  console.log("data-name", `value-${index}`);
+});
