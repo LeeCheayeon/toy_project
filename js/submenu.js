3a657ffbbc30038063066f9ac64d161f1894edc2
leechaeyeon <yeontachi@gmail.com>
2022-06-08 11:09:42 +0900

    innerHTML : test_4

--- js/submenu.js
+++ js/submenu.js
@@ -4,16 +4,6 @@ function box (target, width, height,backgroundColor){
   target.style.backgroundColor = backgroundColor;
 }
 
-
-
-//서브메뉴 겉박스
-// function createSubElement (parentdiv){
-//   const newElement = document.createElement("section");
-//   newElement.setAttribute("id", 'submenu')
-//   box(newElement, '300px','300px','#c4c4c4');
-//   parentdiv.appendChild(newElement);
-// };
-
 const submenu = document.getElementById("submenu");
 const submenus = submenu.children;
 console.log(submenus);
@@ -22,17 +12,17 @@ console.log(submenus);
 // 이미지박스 생성 ==============================
 
 const ImageBox = {
-  "기본": ["이미지-1","이미지-2"],
-  "눈": ["이미지-1","이미지-2","이미지-3"],
-  "코입": ["이미지-1","이미지-2","이미지-3"],
-  "눈썹": ["이미지-1","이미지-2"], 
-  "뒷머리": ["이미지-1","이미지-2"],
-  "앞머리": ["이미지-1","이미지-2","이미지-3"],
-  "귀": ["이미지-1","이미지-2"],
-  "의상": ["이미지-1","이미지-2","이미지-3"],
-  "장식1": ["이미지-1","이미지-2"],
-  "장식2": ["이미지-1","이미지-2","이미지-3"],
-  "뒷장식": ["이미지-1","이미지-2"]
+  "기본": ["base_기본.svg"],
+  "눈": ["base_눈_1.svg","base_눈_2.svg"],
+  "코입": ["base_코입.svg","이미지-2","이미지-3"],
+  "눈썹": ["base_눈썹_1.svg","base_눈썹_2.svg"], 
+  "뒷머리": ["base_뒷머리.svg","이미지-2"],
+  "앞머리": ["base_앞머리.svg","이미지-2","이미지-3"],
+  "귀": ["base_귀.svg","이미지-2"],
+  "의상": ["base_의상.svg","이미지-2","이미지-3"],
+  "장식1": ["base_장식1.svg","이미지-2"],
+  "장식2": ["base_장식2.svg","이미지-2","이미지-3"],
+  "뒷장식": ["base_뒷장식.svg","이미지-2"]
 }
 
 const imageboxArr = Object.values(ImageBox);
@@ -68,57 +58,6 @@ function Deletediv(){
 }
 //-------------------------------------------------
 
-// // 이부분은 회색박스 부분 clickbox의 부모 
-// let isstatus = true;
-// export const clickEvent =  menu.addEventListener("click", function(event){
-//   let getdataset = event.target.dataset.name;
-//   if(isstatus === true){
-//     createSubElement(submenu);
-//       const submenudiv = document.querySelector("#submenu>div");
-//       createDataset(submenudiv, getdataset);
-//           console.log(getdataset);
-//           isstatus = false;
-//         }else{
-//           Deletediv(submenu);
-//           console.log("false");
-//           createSubElement(submenu);
-//           const submenudiv = document.querySelector("#submenu>div");
-//           createDataset(submenudiv, getdataset);
-//         }
-//       })
-      
-
-//       // 이부분이 click box 부분 나눠봤다. 
-//       menu.addEventListener("click", function(evnet){
-//         let getdataset = evnet.target.dataset.name;
-//         const submenudiv = document.querySelector("#submenu>div");
-//         let submenuDataset = submenudiv.dataset.name;
-//         console.log(getdataset);
-//         console.log(submenudiv.dataset.name);
-//         console.dir(evnet.target);
-//         console.dir(ImageBoxArr[0]);
-//         if(getdataset === submenuDataset){
-//           // 만약 getdataset === submenuDataset 같다면
-//           // 새로만든 클릭박스의 데이터셋 이름을 타켓인 디이터셋 이름과 같은걸로 달아줘. 
-//           newClickBox(submenudiv);
-//           let clickBox = document.querySelector("#submenu>div>div");
-//           createDataset(clickBox, getdataset);
-//           console.dir(clickBox.dataset.name);
-//           // 클릭후 dataname이 들어가긴함. 
-//           for(let i=0; i<ImageBoxArr.length; i++){
-            
-//             console.dir(ImageBoxArr[i].length);
-//             if(clickBox.dataset.name === ImageBoxArr[i]){
-//         console.log('true');
-//         console.log(ImageBoxArr[i]);
-//         newClickBox(submenudiv);
-//       }
-//       }
-//       // 만약 데이터 클릭박스의 이름이 메뉴 어레이의 값과 같다면 
-//     }
-    
-//   })
-
 const menu = document.getElementById("menu");
 const menus = menu.children;
 
@@ -146,15 +85,14 @@ export const clickEvent = menu.addEventListener("click", function(event){
         const submenuschild = document.querySelectorAll("#submenu>article>div");
         // console.log(submenuschild);
         for(let i=0; i<submenuschild.length; i++){
+          // 기존에 있던 newdiv 삭제
           Deletediv(submenuschild);
         }
-        // 기존에 있던 newdiv 삭제
         // 다시 newdiv 생성
         for(let j=0; j<imageboxArr.length; j++){
           createnewdiv(submenu.children[i], ImageBox[event.target.dataset.menus][j], 'id', "delete");
           //newdiv 생성 및 undefined display = none이 들어간 함수 
       }
-        // isstatus = true;
         // true로 돌아가면 3번 클릭시 중복생성이 되므로 돌아가지 않는다. 
       }
     }else{
--- js/submenu_main.js
+++ js/submenu_main.js
@@ -1,20 +1,17 @@
-//Q. 그다음 해당 클릭박스를 누르면 main에 이미지가 보이도록 하는 작업도 실행 (생각해보기/검색
-// 1. newdiv 를 클릭하면 main에 이미지가 보이도록해줘 or 새로운 요소를 생성해줘. 
-// 2. newdiv 를 클릭하면 빨강 / 클릭 안 한것은 검정 (event.target)
-// 3. main>section(11)>div(image)
-// main이라는 요소는 id도 main (지정하는데 필요하다면 id 추가)
-// section(11개) id 또는 dataset에 menuArr값처럼 넣기
-// div 생성 : newdiv를 클릭하면 새로운 div 생성
-// submenus에 클릭 이벤트 추가 > event.target(newdiv)를 클릭하면 색상변화 (추후 디자인
-
-
 const submenu = document.getElementById("submenu");
 const submenus = submenu.children;
 console.log(submenus);
 const submenuschild = document.querySelectorAll("#submenu>article>div");
 console.log(submenuschild);
 
-export const imageMain = submenus.addEaddEventListener("click", function(event){
-  console.log(event.target);
+//==================================================
+const main = document.querySelector("main");
+console.log(main);
+//==================================================
+
+
+export const imageMain = 
+submenus.addEaddEventListener("click", function(){
+  console.log("click");
 
 });
